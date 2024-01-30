5913e7fe7607abbebf3969bdb48367de9b4d5205
Jason9265 <fjs971030@gmail.com>
2024-01-30 21:20:55 +1030

    draft extension

---
+++ src/workflow/workFlow.tsx
@@ -0,0 +1,62 @@
+import React, { useEffect } from 'react';
+import { createRoot } from 'react-dom/client';
+
+
+const WorkFlow = () => {
+  // Function to update the turbo frame
+  const updateTurboFrame = () => {
+    const turboFrame = document.querySelector('#repo-content-turbo-frame');
+    if (turboFrame) {
+      turboFrame.innerHTML = '<p>New Content</p>';
+    }
+  };
+
+  // Function to add the Workflow tab
+  const addWorkFlowTab = () => {
+    const navBar = document.querySelector('.UnderlineNav-body');
+    if (navBar && !document.getElementById('workflow-tab')) {
+      const container = document.createElement('li');
+      container.className = 'd-inline-flex';
+
+      const imagePath = chrome.runtime.getURL('workflow.svg');
+
+      // Render component into the container
+      const root = createRoot(container);
+      root.render(
+        <a id="workflow-tab" 
+          className="UnderlineNav-item no-wrap js-responsive-underlinenav-item"
+          onClick={updateTurboFrame}>
+            <img src={imagePath}  style={{ width: '16px', height: '16px', verticalAlign: 'middle' }} />
+            Work Flow
+        </a>
+      );
+
+      navBar.appendChild(container);
+    }
+  };
+
+  // Effect hook to set up the MutationObserver
+  useEffect(() => {
+    const observer = new MutationObserver(mutations => {
+      mutations.forEach(mutation => {
+        if (mutation.addedNodes.length) {
+          addWorkFlowTab();
+        }
+      });
+    });
+
+    observer.observe(document.body, { childList: true, subtree: true });
+
+    return () => observer.disconnect();
+  }, []);
+
+  // Add the tab
+  useEffect(() => {
+    addWorkFlowTab();
+  }, []);
+
+  return null;
+};
+
+
+export default WorkFlow;
---
+++ webpack.common.js
@@ -0,0 +1,82 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlPlugin = require('html-webpack-plugin');
+const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+    mode: "development",
+    devtool: 'cheap-module-source-map',
+    entry: {
+        popup: path.resolve('./src/popup/popup.tsx'),
+        contentScript: path.resolve('./src/workflow/index.tsx')
+    },
+    module: {
+        rules: [
+            {
+                use: "ts-loader",
+                test: /\.tsx$/,
+                exclude: /node_modules/
+            },
+            {
+                use: ['style-loader', 'css-loader', {
+                    loader: 'postcss-loader',
+                    options: {
+                        postcssOptions: {
+                            ident: 'postcss',
+                            plugins: [tailwindcss, autoprefixer],
+                        }
+                    }
+                }],
+                test: /\.css$/i,
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|woff|woff2|tff|eot|svg)$/,
+                type: 'asset/resource',
+            }
+        ]
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve('src/static/manifest.json'), 
+                    to: path.resolve('dist')
+                },
+                {
+                    from: path.resolve('src/static/icon48.png'), 
+                    to: path.resolve('dist')
+                },
+                {
+                    from: path.resolve('src/static/workflow.svg'), 
+                    to: path.resolve('dist')
+                },
+            ]
+        }),
+        ...getHtmlPlugins([
+            'popup',
+        ])
+    ],
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    output: {
+        filename: "[name].js",
+        assetModuleFilename: '[name][ext]'
+    },
+    optimization: {
+        splitChunks: {
+            chunks(chunk) {
+                return chunk.name !== 'contentScript';
+            }
+        }
+    }
+}
+
+function getHtmlPlugins(chunks) {
+    return chunks.map(chunk => new HtmlPlugin({
+        title: 'My extension',
+        filename: `${chunk}.html`,
+        chunks: [chunk]
+    }))
+}
