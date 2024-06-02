db072a316111a6bfbc5baa3fd9f0b1e9d0af4078
Jason9265 <fjs971030@gmail.com>
2024-06-02 14:18:47 +0930

    update flashing icon href to running or latest workflow

--- src/github-access/github-access.tsx
+++ src/github-access/github-access.tsx
@@ -18,9 +18,9 @@ export const getStorageData = (): Promise<StorageResult> => {
     });
 };
 
-export const fetchData = async (octokit, info, setIsFlashing, setShouldPoll, setHeadBranch, setHoverInfo) => {
+export const fetchData = async (octokit, info, setIsFlashing, setShouldPoll, setIconHref, setHoverInfo) => {
     try {
-        setIsFlashing(await accessGitHub(info, octokit, setHeadBranch, setHoverInfo));
+        setIsFlashing(await accessGitHub(info, octokit, setIconHref, setHoverInfo));
     } catch (error) {
         if (error.status === 401) {
             console.error('Error: Bad credentials. Please check your GitHub Token.');
@@ -31,19 +31,33 @@ export const fetchData = async (octokit, info, setIsFlashing, setShouldPoll, set
     }
 };
 
-export const accessGitHub = async (info, octokit, setHeadBranch, setHoverInfo) => {
+const accessGitHub = async (info, octokit, setIconHref, setHoverInfo) => {
     const { data } = await octokit.request('GET /repos/{owner}/{repo}/actions/runs', {
         owner: info.ownerName,
         repo: info.repoName
     })
 
-    const runningWorkflow = data.workflow_runs.find(run => run.status === "in_progress" || run.status === "queued" || run.status === "waiting");
-    setHeadBranch(runningWorkflow ? runningWorkflow.head_branch: '');
-
+    const runningWorkflow = data.workflow_runs.find(run => run.status === "in_progress");
+    const queuedWorkflow = data.workflow_runs.find(run => run.status === "queued" || run.status === "waiting");
+    const firstWorkflowRunId = data.workflow_runs[0];
     if (runningWorkflow) {
+        // return running workflow run id
+        setIconHref("runs/" + runningWorkflow.id);
+
         const createdAt = new Date(runningWorkflow.created_at);
         const runningTime = formatDistanceToNow(createdAt);
         setHoverInfo(`Workflow is running\nRunning time: ${runningTime}.`);
+    } else if (queuedWorkflow) {
+        setIconHref("runs/" + queuedWorkflow.id);
+
+        const createdAt = new Date(queuedWorkflow.created_at);
+        const runningTime = formatDistanceToNow(createdAt);
+        setHoverInfo(`Workflow is running\nRunning time: ${runningTime}.`);
+    } else if (firstWorkflowRunId) {
+        // return the latest workflow run id
+        setIconHref("runs/" + firstWorkflowRunId.id);
+    } else {
+        setIconHref('');
     }
 
     return !!runningWorkflow;
